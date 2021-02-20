bf17f01fd194e4aee345ab14979ae7cd2c9661f5
Benjamin Cheng <ben@bcheng.me>
2021-02-20 01:15:10 -0500

    client: seprate canvas code into a class

---
+++ client/src/canvas.ts
@@ -0,0 +1,78 @@
+interface Pos {
+    x: number;
+    y: number;
+};
+
+export class DrawingCanvas {
+    canvas: HTMLCanvasElement;
+    ctx: CanvasRenderingContext2D;
+
+    /* rendering state */
+    private lastframe: number = 0;
+
+    /* drawing state */
+    private drawing: boolean = false;
+    private lastpos: Pos = {x: 0, y: 0};
+    private curpos: Pos = {x: 0, y: 0};
+
+    constructor(element: HTMLCanvasElement) {
+        const ctx = element.getContext('2d');
+        if (ctx == null) {
+            throw "Fuck off";
+        }
+
+        this.canvas = element;
+        this.ctx = ctx;
+
+        this.ctx.strokeStyle = '#000000';
+        this.ctx.lineWidth = 2;
+        this.initEventHandlers();
+    }
+
+    private initEventHandlers() {
+        this.canvas.addEventListener('mousedown', e => {
+            this.drawing = true;
+            this.lastpos.x = e.x;
+            this.lastpos.y = e.y;
+        });
+        this.canvas.addEventListener('mouseup', e => {
+            this.drawing = false;
+        });
+        this.canvas.addEventListener('mousemove', e => {
+            this.curpos.x = e.x;
+            this.curpos.y = e.y;
+        });
+        this.canvas.addEventListener('touchstart', e => {
+            this.drawing = true;
+            const touch = e.touches[0];
+
+            this.lastpos.x = touch.clientX;
+            this.lastpos.y = touch.clientY;
+        });
+        this.canvas.addEventListener('touchend', e => {
+            this.drawing = false;
+        });
+        this.canvas.addEventListener('touchmove', e => {
+            const touch = e.touches[0];
+
+            this.curpos.x = touch.clientX;
+            this.curpos.y = touch.clientY;
+        });
+    }
+
+    render(timestamp: number) {
+        let delta = timestamp - this.lastframe;
+        this.lastframe = timestamp;
+
+        console.log(delta);
+        if (this.drawing) {
+            this.ctx.moveTo(this.lastpos.x, this.lastpos.y);
+            this.ctx.lineTo(this.curpos.x, this.curpos.y);
+            this.ctx.stroke();
+            this.lastpos.x = this.curpos.x;
+            this.lastpos.y = this.curpos.y;
+        }
+
+    }
+
+};
--- client/src/index.ts
+++ client/src/index.ts
@@ -1,63 +1,12 @@
-const canvas = document.getElementById('thecanvas')! as HTMLCanvasElement;
-const ctx = canvas.getContext('2d')!;
-ctx.strokeStyle = '#000000';
-ctx.lineWidth = 2;
+import { DrawingCanvas } from './canvas';
 
-ctx.moveTo(0, 0);
-ctx.lineTo(10, 10);
-ctx.stroke();
-
-let drawing = false;
-const lastpos = {x: 0, y: 0};
-const curpos = {x: 0, y: 0};
-
-canvas.addEventListener('mousedown', e => {
-    drawing = true;
-    lastpos.x = e.x;
-    lastpos.y = e.y;
-});
-canvas.addEventListener('mouseup', e => {
-    drawing = false;
-});
-canvas.addEventListener('mousemove', e => {
-    curpos.x = e.x;
-    curpos.y = e.y;
-});
-canvas.addEventListener('touchstart', e => {
-    drawing = true;
-    const touch = e.touches[0];
-    // console.log(e.touches.length);
-    // console.log(touch);
-
-    lastpos.x = touch.clientX;
-    lastpos.y = touch.clientY;
-});
-canvas.addEventListener('touchend', e => {
-    drawing = false;
-});
-canvas.addEventListener('touchmove', e => {
-    const touch = e.touches[0];
-    // console.log(e.touches.length);
-    // console.log(touch);
-
-    curpos.x = touch.clientX;
-    curpos.y = touch.clientY;
-});
-
-let lastframe = 0;
-function renderFrame(timestamp: number) {
-    window.requestAnimationFrame(renderFrame);
-    let delta = timestamp - lastframe;
-    timestamp = lastframe;
-
-    console.log(delta, drawing);
-    if (drawing) {
-        ctx.moveTo(lastpos.x, lastpos.y);
-        ctx.lineTo(curpos.x, curpos.y);
-        ctx.stroke();
-        lastpos.x = curpos.x;
-        lastpos.y = curpos.y;
-    }
+const element = document.getElementById('thecanvas') as HTMLCanvasElement;
+if (element == null) {
+    throw "Fuck you";
 }
+const canvas = new DrawingCanvas(element);
 
-window.requestAnimationFrame(renderFrame);
+window.requestAnimationFrame((function cb(timestamp) {
+    window.requestAnimationFrame(cb);
+    canvas.render(timestamp);
+}));
