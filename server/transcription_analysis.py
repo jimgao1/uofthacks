9e2a9b340ff7bdd54017269a06ce17184dfe6b71
uoftcspost <jasonliuedu@gmail.com>
2021-02-21 00:50:37 -0500

    oof

--- server/transcription_analysis.py
+++ server/transcription_analysis.py
@@ -132,8 +132,65 @@ def listen_print_loop(responses):
 
         else:
             message = transcript + overwrite_chars
-            print(message)
-            transcript_list.append({'message': message, 'time': time.perf_counter() - start_time})
+            print("sentence:", message)
+
+            sentence_info = {}
+            
+            sentence_info['message'] = message
+
+
+                ####
+            # sentiment analysis
+
+            print(transcript_list)
+
+            # meeting_text = ""
+            # for oof in transcript_list:
+            #     meeting_text += oof['message']
+
+            # Instantiates a client
+            client = language_v1.LanguageServiceClient()
+
+            # The text to analyze
+            text = message
+            document = language_v1.Document(content=text, type_=language_v1.Document.Type.PLAIN_TEXT)
+
+            # Detects the sentiment of the text
+            sentiment = client.analyze_sentiment(request={'document': document}).document_sentiment
+            # entities = client.analyze_entities(request={'document': document}).entities
+
+            print("Text: {}".format(text))
+            print("Sentiment: {}, {}".format(sentiment.score, sentiment.magnitude))
+
+            sentence_info['sentiment'] = sentiment.score
+
+            ####
+            # entity recognition
+
+            # Available types: PLAIN_TEXT, HTML
+            type_ = language_v1.Document.Type.PLAIN_TEXT
+
+            # Optional. If not specified, the language is automatically detected.
+            # For list of supported languages:
+            # https://cloud.google.com/natural-language/docs/languages
+            language = "en"
+            document = {"content": text_content, "type_": type_, "language": language}
+
+            # Available values: NONE, UTF8, UTF16, UTF32
+            encoding_type = language_v1.EncodingType.UTF8
+
+            response = client.analyze_entities(request = {'document': document, 'encoding_type': encoding_type})
+
+            # Loop through entitites returned from the API
+
+            entity = response.entities[0]
+
+            print(u"keyword of the sentence: {}".format(entity.name))
+
+            sentence_info['keyword'] = entity.name 
+
+
+            transcript_list.append(sentence_info)
 
             # Exit recognition if any of the transcribed phrases could be
             # one of our keywords.
@@ -142,84 +199,6 @@ def listen_print_loop(responses):
                 break
 
             num_chars_printed = 0
-
-
-    ####
-    # sentiment analysis
-
-    print(transcript_list)
-
-    meeting_text = ""
-    for oof in transcript_list:
-        meeting_text += oof['message']
-
-    # Instantiates a client
-    client = language_v1.LanguageServiceClient()
-
-    # The text to analyze
-    text = meeting_text
-    document = language_v1.Document(content=text, type_=language_v1.Document.Type.PLAIN_TEXT)
-
-    # Detects the sentiment of the text
-    sentiment = client.analyze_sentiment(request={'document': document}).document_sentiment
-    # entities = client.analyze_entities(request={'document': document}).entities
-
-    print("Text: {}".format(text))
-    print("Sentiment: {}, {}".format(sentiment.score, sentiment.magnitude))
-
-
-
-    ####
-    # entity recognition
-
-    # Available types: PLAIN_TEXT, HTML
-    type_ = language_v1.Document.Type.PLAIN_TEXT
-
-    # Optional. If not specified, the language is automatically detected.
-    # For list of supported languages:
-    # https://cloud.google.com/natural-language/docs/languages
-    language = "en"
-    document = {"content": meeting_text, "type_": type_, "language": language}
-
-    # Available values: NONE, UTF8, UTF16, UTF32
-    encoding_type = language_v1.EncodingType.UTF8
-
-    response = client.analyze_entities(request={'document': document, 'encoding_type': encoding_type})
-
-    # Loop through entitites returned from the API
-    for entity in response.entities:
-        print(u"Representative name for the entity: {}".format(entity.name))
-
-        # Get entity type, e.g. PERSON, LOCATION, ADDRESS, NUMBER, et al
-        print(u"Entity type: {}".format(language_v1.Entity.Type(entity.type_).name))
-
-        # Get the salience score associated with the entity in the [0, 1.0] range
-        print(u"Salience score: {}".format(entity.salience))
-
-        # Loop over the metadata associated with entity. For many known entities,
-        # the metadata is a Wikipedia URL (wikipedia_url) and Knowledge Graph MID (mid).
-        # Some entity types may have additional metadata, e.g. ADDRESS entities
-        # may have metadata for the address street_name, postal_code, et al.
-        for metadata_name, metadata_value in entity.metadata.items():
-            print(u"{}: {}".format(metadata_name, metadata_value))
-
-        # Loop over the mentions of this entity in the input document.
-        # The API currently supports proper noun mentions.
-        for mention in entity.mentions:
-            print(u"Mention text: {}".format(mention.text.content))
-
-            # Get the mention type, e.g. PROPER for proper noun
-            print(
-                u"Mention type: {}".format(language_v1.EntityMention.Type(mention.type_).name)
-            )
-
-    # Get the language of the text, which will be the same as
-    # the language specified in the request or, if not specified,
-    # the automatically-detected language.
-    print(u"Language of the text: {}".format(response.language))
-
-
-
 
 
 def main():
@@ -253,116 +232,116 @@ def main():
 
 
 if __name__ == "__main__":
-    transcript_list = [{'message': "what's the actual suck", 'time': 3.7776398999999996},
-            {'message': ' is it possible', 'time': 28.997634599999998},
-            {'message': ' this is private right what can deposit something into this', 'time': 37.842910599999996},
-            {'message': ' quit', 'time': 56.5095233}] * 10
+    # transcript_list = [{'message': "what's the actual suck", 'time': 3.7776398999999996},
+    #         {'message': ' is it possible', 'time': 28.997634599999998},
+    #         {'message': ' this is private right what can deposit something into this', 'time': 37.842910599999996},
+    #         {'message': ' quit', 'time': 56.5095233}] * 10
 
-    meeting_text = ""
-    for oof in transcript_list:
-        meeting_text += oof['message']
-
-
-    client = language_v1.LanguageServiceClient()
-    # text_content = 'California is a state.'
-
-    ####
-    # sentiment analysis
-
-    print(transcript_list)
-
-    meeting_text = ""
-    for oof in transcript_list:
-        meeting_text += oof['message']
-
-    # Instantiates a client
-    client = language_v1.LanguageServiceClient()
-
-    # The text to analyze
-    text = meeting_text
-    document = language_v1.Document(content=text, type_=language_v1.Document.Type.PLAIN_TEXT)
-
-    # Detects the sentiment of the text
-    sentiment = client.analyze_sentiment(request={'document': document}).document_sentiment
-    # entities = client.analyze_entities(request={'document': document}).entities
-
-    print("Text: {}".format(text))
-    print("Sentiment: {}, {}".format(sentiment.score, sentiment.magnitude))
-
-    ####
-    # entity recognition
-
-    # Available types: PLAIN_TEXT, HTML
-    type_ = language_v1.Document.Type.PLAIN_TEXT
-
-    # Optional. If not specified, the language is automatically detected.
-    # For list of supported languages:
-    # https://cloud.google.com/natural-language/docs/languages
-    language = "en"
-    document = {"content": meeting_text, "type_": type_, "language": language}
-
-    # Available values: NONE, UTF8, UTF16, UTF32
-    encoding_type = language_v1.EncodingType.UTF8
-
-    response = client.analyze_entities(request={'document': document, 'encoding_type': encoding_type})
-
-    # Loop through entitites returned from the API
-    for entity in response.entities:
-        print(u"Representative name for the entity: {}".format(entity.name))
-
-        # Get entity type, e.g. PERSON, LOCATION, ADDRESS, NUMBER, et al
-        print(u"Entity type: {}".format(language_v1.Entity.Type(entity.type_).name))
-
-        # Get the salience score associated with the entity in the [0, 1.0] range
-        print(u"Salience score: {}".format(entity.salience))
-
-        # Loop over the metadata associated with entity. For many known entities,
-        # the metadata is a Wikipedia URL (wikipedia_url) and Knowledge Graph MID (mid).
-        # Some entity types may have additional metadata, e.g. ADDRESS entities
-        # may have metadata for the address street_name, postal_code, et al.
-        for metadata_name, metadata_value in entity.metadata.items():
-            print(u"{}: {}".format(metadata_name, metadata_value))
-
-        # Loop over the mentions of this entity in the input document.
-        # The API currently supports proper noun mentions.
-        for mention in entity.mentions:
-            print(u"Mention text: {}".format(mention.text.content))
-
-            # Get the mention type, e.g. PROPER for proper noun
-            print(
-                u"Mention type: {}".format(language_v1.EntityMention.Type(mention.type_).name)
-            )
-
-    # Get the language of the text, which will be the same as
-    # the language specified in the request or, if not specified,
-    # the automatically-detected language.
-    print(u"Language of the text: {}".format(response.language))
+    # meeting_text = ""
+    # for oof in transcript_list:
+    #     meeting_text += oof['message']
 
 
-    ####
-    # classify content
+    # client = language_v1.LanguageServiceClient()
+    # # text_content = 'California is a state.'
 
-    # text_content = 'That actor on TV makes movies in Hollywood and also stars in a variety of popular new TV shows.'
+    # ####
+    # # sentiment analysis
 
-    # Available types: PLAIN_TEXT, HTML
-    type_ = language_v1.Document.Type.PLAIN_TEXT
+    # print(transcript_list)
 
-    # Optional. If not specified, the language is automatically detected.
-    # For list of supported languages:
-    # https://cloud.google.com/natural-language/docs/languages
-    language = "en"
-    document = {"content": meeting_text, "type_": type_, "language": language}
+    # meeting_text = ""
+    # for oof in transcript_list:
+    #     meeting_text += oof['message']
 
-    response = client.classify_text(request = {'document': document})
-    # Loop through classified categories returned from the API
-    for category in response.categories:
-        # Get the name of the category representing the document.
-        # See the predefined taxonomy of categories:
-        # https://cloud.google.com/natural-language/docs/categories
-        print(u"Category name: {}".format(category.name))
-        # Get the confidence. Number representing how certain the classifier
-        # is that this category represents the provided text.
-        print(u"Confidence: {}".format(category.confidence))
+    # # Instantiates a client
+    # client = language_v1.LanguageServiceClient()
+
+    # # The text to analyze
+    # text = meeting_text
+    # document = language_v1.Document(content=text, type_=language_v1.Document.Type.PLAIN_TEXT)
+
+    # # Detects the sentiment of the text
+    # sentiment = client.analyze_sentiment(request={'document': document}).document_sentiment
+    # # entities = client.analyze_entities(request={'document': document}).entities
+
+    # print("Text: {}".format(text))
+    # print("Sentiment: {}, {}".format(sentiment.score, sentiment.magnitude))
+
+    # ####
+    # # entity recognition
+
+    # # Available types: PLAIN_TEXT, HTML
+    # type_ = language_v1.Document.Type.PLAIN_TEXT
+
+    # # Optional. If not specified, the language is automatically detected.
+    # # For list of supported languages:
+    # # https://cloud.google.com/natural-language/docs/languages
+    # language = "en"
+    # document = {"content": meeting_text, "type_": type_, "language": language}
+
+    # # Available values: NONE, UTF8, UTF16, UTF32
+    # encoding_type = language_v1.EncodingType.UTF8
+
+    # response = client.analyze_entities(request={'document': document, 'encoding_type': encoding_type})
+
+    # # Loop through entitites returned from the API
+    # for entity in response.entities:
+    #     print(u"Representative name for the entity: {}".format(entity.name))
+
+    #     # Get entity type, e.g. PERSON, LOCATION, ADDRESS, NUMBER, et al
+    #     print(u"Entity type: {}".format(language_v1.Entity.Type(entity.type_).name))
+
+    #     # Get the salience score associated with the entity in the [0, 1.0] range
+    #     print(u"Salience score: {}".format(entity.salience))
+
+    #     # Loop over the metadata associated with entity. For many known entities,
+    #     # the metadata is a Wikipedia URL (wikipedia_url) and Knowledge Graph MID (mid).
+    #     # Some entity types may have additional metadata, e.g. ADDRESS entities
+    #     # may have metadata for the address street_name, postal_code, et al.
+    #     for metadata_name, metadata_value in entity.metadata.items():
+    #         print(u"{}: {}".format(metadata_name, metadata_value))
+
+    #     # Loop over the mentions of this entity in the input document.
+    #     # The API currently supports proper noun mentions.
+    #     for mention in entity.mentions:
+    #         print(u"Mention text: {}".format(mention.text.content))
+
+    #         # Get the mention type, e.g. PROPER for proper noun
+    #         print(
+    #             u"Mention type: {}".format(language_v1.EntityMention.Type(mention.type_).name)
+    #         )
+
+    # # Get the language of the text, which will be the same as
+    # # the language specified in the request or, if not specified,
+    # # the automatically-detected language.
+    # print(u"Language of the text: {}".format(response.language))
+
+
+    # ####
+    # # classify content
+
+    # # text_content = 'That actor on TV makes movies in Hollywood and also stars in a variety of popular new TV shows.'
+
+    # # Available types: PLAIN_TEXT, HTML
+    # type_ = language_v1.Document.Type.PLAIN_TEXT
+
+    # # Optional. If not specified, the language is automatically detected.
+    # # For list of supported languages:
+    # # https://cloud.google.com/natural-language/docs/languages
+    # language = "en"
+    # document = {"content": meeting_text, "type_": type_, "language": language}
+
+    # response = client.classify_text(request = {'document': document})
+    # # Loop through classified categories returned from the API
+    # for category in response.categories:
+    #     # Get the name of the category representing the document.
+    #     # See the predefined taxonomy of categories:
+    #     # https://cloud.google.com/natural-language/docs/categories
+    #     print(u"Category name: {}".format(category.name))
+    #     # Get the confidence. Number representing how certain the classifier
+    #     # is that this category represents the provided text.
+    #     print(u"Confidence: {}".format(category.confidence))
 
 
 
